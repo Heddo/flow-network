296863e156ad2f54598cc3ac5b043d4e0f676f6b
mastercoms <circuitsoft@tuta.io>
2016-03-05 11:41:15 -0500

    Fix Javadocs warnings

--- src/main/java/com/flowpowered/network/processor/MessageProcessor.java
+++ src/main/java/com/flowpowered/network/processor/MessageProcessor.java
@@ -27,7 +27,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 
 /**
- * {@code MessageProcessor} can be used in a {@link com.flowpowered.networking.pipeline.MessageDecoder} or {@link com.flowpowered.networking.pipeline.MessageEncoder} to define
+ * {@code MessageProcessor} can be used in a {@link com.flowpowered.network.pipeline.MessageDecoder} or {@link com.flowpowered.network.pipeline.MessageEncoder} to define
  * how a {@code ByteBuf} should be processed prior to decode or after encode.
  */
 public interface MessageProcessor {
